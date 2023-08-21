d7edbcd9ac53fa4200d22741665692910eb855ef
Trenchi (Maik) <redbug@gmx.de>
2023-08-21 15:25:04 +0200

    Rework Errors

--- after_install.sh
+++ after_install.sh
@@ -6,8 +6,8 @@ sudo apt upgrade
 sudo apt install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-mv index.html /var/www/html
-chown www-data:www-data /var/www/html
-chown www-data:www-data /var/www/html/index.html
+sudo mv index.html /var/www/html
+sudo chown www-data:www-data /var/www/html
+sudo chown www-data:www-data /var/www/html/index.html
 sudo rm -r index.html
 touch FERTIG
