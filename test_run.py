7dc64959962a81eb85bece1c996a0f6b0e0cb44b
Trenchi (Maik) <redbug@gmx.de>
2023-08-21 13:37:07 +0200

    Seccond Error Fixing

--- test_run.py
+++ test_run.py
@@ -6,8 +6,8 @@ from run import add, uppercase
 class TestAdd(unittest.TestCase):
 
     def test_add_function(self):
-        self.assertEqual(add(2, 2), 5)
-        self.assertEqual(add(4, 4), 17)
+        self.assertEqual(add(2, 2), 4)
+        self.assertEqual(add(4, 4), 16)
 
     def text_uppercase_make_it_uppercase(self):
         self.assertEqual(uppercase("Baum"), "BAUM")
