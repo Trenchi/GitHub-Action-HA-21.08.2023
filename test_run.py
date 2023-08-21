ce2e15207aaf1ddcd15ba52e7481cd3fd66ac9a1
Trenchi (Maik) <redbug@gmx.de>
2023-08-21 13:46:12 +0200

    Rework Unittest

--- test_run.py
+++ test_run.py
@@ -6,7 +6,7 @@ from run import add, uppercase
 class TestAdd(unittest.TestCase):
 
     def test_add_function(self):
-        self.assertEqual(add(2, 2), 4)
+        self.assertEqual(add(2, 3), 6)
         self.assertEqual(add(4, 4), 16)
 
     def text_uppercase_make_it_uppercase(self):
